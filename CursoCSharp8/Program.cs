6e9a8036f8b2a2f05d4ced2cda1611a167330213
Santiago <gomezgu31@gmail.com>
2023-01-08 23:18:52 -0600

    New class

--- CursoCSharp8/Program.cs
+++ CursoCSharp8/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace CursoCSharp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
